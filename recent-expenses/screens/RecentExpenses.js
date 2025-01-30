12904273bc5ce2cc81681e2ffe2896812a5aabff
abdc06 <abdc2806@gmail.com>
2025-01-30 09:13:23 +0900

    Screen 구조 작업

---
+++ recent-expenses/screens/RecentExpenses.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function RecentExpenses() {
+  return (
+    <View>
+      <Text>RecentExpenses Screens</Text>
+    </View>
+  );
+}
+
+export default RecentExpenses;
